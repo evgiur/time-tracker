a2d2a5ef63746748e192f1c6f327f2b38b4c2a31
evgiur <evgiur@gmail.com>
2024-05-21 01:58:28 +0300

    font-added

--- src/components/Maincart.jsx
+++ src/components/Maincart.jsx
@@ -4,7 +4,8 @@ import '../scss/app.scss';
 export default function Maincart() {
   return (
     <div className='main-cart-background'>
-      <header class="red-text">
+      <header>
+        <img src="image-jeremy.png" alt="jeremy"></img>
         Report for 
         Jeremy Robson
       </header>
