f89cc92aff1172ca1e6737e03a5f7c389f28398b
evgiur <evgiur@gmail.com>
2024-05-26 20:49:36 +0300

    scss-changed

--- src/components/Maincart.jsx
+++ src/components/Maincart.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import '../scss/app.scss';
+import avatar from '../assets/images/image-jeremy.png';
 
 export default function Maincart() {
   return (
     <div className='main-cart-background'>
+      <div className='image-container'>
+        <img src={avatar} alt="jeremy"></img>
+      </div>
       <header>
-        <img src="image-jeremy.png" alt="jeremy"></img>
-        Report for 
-        Jeremy Robson
+        <h1>Report for</h1>
+        <h2>Jeremy Robson</h2>
       </header>
-      <list>
-        Daily
-        Weekly
-        Monthly
-      </list>  
+      <div> 
+        <ul>
+          <li>Daily</li>
+          <li>Weekly</li>
+          <li>Monthly</li>
+        </ul>
+      </div>
+      
     </div>
   )
 }
