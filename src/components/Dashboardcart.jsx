d16c0c3a40913a136607e5d3f3ce09f90a8e4a6e
evgiur <evgiur@gmail.com>
2024-06-15 22:59:49 +0300

    scss-edit

--- src/components/Dashboardcart.jsx
+++ src/components/Dashboardcart.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import '../scss/app-copy.scss';
 
 
 export default function Dashboardcart() {
--- src/components/Maincart.jsx
+++ src/components/Maincart.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import '../scss/app.scss';
+import '../scss/app-copy.scss';
 import avatar from '../assets/images/image-jeremy.png';
 
 export default function Maincart() {
   return (
-    <div className='main-cart-background'>
-      <div className='image-container'>
-        <img src={avatar} alt="jeremy"></img>
-      </div>
-      <header>
-        <h1>Report for</h1>
-        <h2>Jeremy Robson</h2>
-      </header>
-      <div className='tabs'> 
-        <button id="daily-button">Daily</button>
-        <button id="weekly-button">Weekly</button>
-        <button id="monthly-button">Monthly</button>
-      </div>
+    <div className='main-cart'>
       
+      <div className='top-block'>
+        
+        <div className='image-container'>
+          <img src={avatar} alt="jeremy"></img>
+        </div>
+      
+        <header>
+          <h1>Report for</h1>
+          <h2>Jeremy Robson</h2>
+        </header>
+
+      </div>
+
+      <div className='tabs'> 
+        <div className='buttons'> 
+          <button id="daily-button">Daily</button>
+          <button id="weekly-button">Weekly</button>
+          <button id="monthly-button">Monthly</button>
+        </div>
+      </div>
+
     </div>
   )
 }
