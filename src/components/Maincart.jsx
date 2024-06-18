06b9ebb6601d332d08348515736e2918de268e6b
evgiur <evgiur@gmail.com>
2024-06-18 07:07:07 +0300

    scss-edited

--- src/components/Maincart.jsx
+++ src/components/Maincart.jsx
@@ -5,28 +5,21 @@ import avatar from '../assets/images/image-jeremy.png';
 export default function Maincart() {
   return (
     <div className='main-cart'>
-      
-      <div className='top-block'>
-        
         <div className='image-container'>
           <img src={avatar} alt="jeremy"></img>
+  
+          <header>
+            <h1>Report for</h1>
+            <h2>Jeremy Robson</h2>
+          </header>
+
         </div>
-      
-        <header>
-          <h1>Report for</h1>
-          <h2>Jeremy Robson</h2>
-        </header>
-
-      </div>
-
-      <div className='tabs'> 
-        <div className='buttons'> 
+    
+        <div className='buttons'>
           <button id="daily-button">Daily</button>
           <button id="weekly-button">Weekly</button>
           <button id="monthly-button">Monthly</button>
         </div>
-      </div>
-
     </div>
   )
 }
